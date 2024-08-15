249f5a0dac60cb624ed412ede0472dd5f43f3ed3
noprizal <noprizal@voltras.co.id>
2024-08-15 18:18:29 +0700

    setup controller

--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex flex-row justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl dark:text-slate-100 text-gray-800 leading-tight">
                 {{ __('Manage Statistics') }}
             </h2>
-            <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+            <a href="" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                 Add New
             </a>
         </div>
@@ -12,7 +12,7 @@
     
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
+            <div class="dark:bg-slate-200 bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
  
                 <div class="item-card flex flex-row justify-between items-center">
                     <div class="flex flex-row items-center gap-x-3">
@@ -23,7 +23,7 @@
                     </div> 
                     <div  class="hidden md:flex flex-col">
                         <p class="text-slate-500 text-sm">Date</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">asdasdsad}</h3>
+                        <h3 class="text-indigo-950 text-xl font-bold">asdasdsad</h3>
                     </div>
                     <div class="hidden md:flex flex-row items-center gap-x-3">
                         <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
