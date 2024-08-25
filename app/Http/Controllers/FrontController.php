7845f19fa7f440d7630ccce2175788a09927f758
noprizal <noprizal@voltras.co.id>
2024-08-25 21:43:29 +0700

    add about page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyAbout;
 use App\Models\CompanyStatistic;
 use App\Models\HeroSection;
 use App\Models\OurPrinciple;
@@ -13,19 +14,26 @@ class FrontController extends Controller
 {
     public function index()
     {
-        $statistics = CompanyStatistic::take(4)->get();
+        $statistics = CompanyStatistic::orderByDesc('id')->take(4)->get();
         $principles = OurPrinciple::take(4)->get();
         $products = Product::take(3)->get();
         $teams = OurTeam::take(7)->get();
         $testimonials = Testimonial::take(4)->get();
-        $heroSections = HeroSection::orderByDesc('id')->take(1)->get();
+        $heroSections = HeroSection::take(1)->get();
         return view('front.index', compact('statistics', 'principles', 'products', 'teams', 'testimonials', 'heroSections'));
     }
 
     public function team()
     {
         $teams = OurTeam::get();
-        $statistics = CompanyStatistic::take(4)->get();
+        $statistics = CompanyStatistic::orderByDesc('id')->take(4)->get();
         return view('front.team', compact('teams', 'statistics'));
+    }
+
+    public function about()
+    {
+        $abouts = CompanyAbout::take(2)->get();
+        $statistics = CompanyStatistic::orderByDesc('id')->take(4)->get();
+        return view('front.about', compact('abouts', 'statistics'));
     }
 }
